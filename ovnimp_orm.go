f90f8a448f09fc6cbf47521bd42f5f149e086acb
Adrian Moreno <amorenoz@redhat.com>
2021-03-04 15:30:00 +0100

    orm: add generic Get() function
    Leverage libovsdb's ORMAPI.Equal feature
    
    Signed-off-by: Adrian Moreno <amorenoz@redhat.com>

--- ovnimp_orm.go
+++ ovnimp_orm.go
@@ -39,6 +39,37 @@ func (odbi *ovndb) GetByID(result interface{}, uuid string) error {
 	resultVal.Elem().Set(reflect.Indirect(reflect.ValueOf(elem)))
 	return nil
 }
+
+// Get is a generic Get function capable of returning (through a provided pointer)
+// a instance of any row in the cache. It only works on ORM mode.
+// 'result' must be a pointer to an Model that exists in the DBModel
+//
+// The way the cache is search depends on the fields already populated in 'result'
+// Any table index (including _uuid) will be used for comparison. Additionally,
+// any additional index provided as argument will be also used
+func (odbi *ovndb) Get(model Model, index ...string) error {
+	if odbi.mode != ORM {
+		return fmt.Errorf("Get() is only available in ORM mode")
+	}
+
+	resultVal := reflect.ValueOf(model)
+	if resultVal.Type().Kind() != reflect.Ptr {
+		return fmt.Errorf("Get() result must be a pointer")
+	}
+
+	table := odbi.findTable(resultVal.Type())
+	if table == "" {
+		return ErrorSchema
+	}
+
+	elem, err := odbi.ormFindInCache(table, model, index...)
+	if err != nil {
+		return err
+	}
+	resultVal.Elem().Set(reflect.Indirect(reflect.ValueOf(elem)))
+	return nil
+
+}
 func (odbi *ovndb) findTable(mType reflect.Type) TableName {
 	for table, tType := range odbi.dbModel.types {
 		if tType == mType {
@@ -55,4 +86,34 @@ func (odbi *ovndb) setUUID(model Model, uuid string) {
 
 func (odbi *ovndb) getUUID(model Model) string {
 	return reflect.Indirect(reflect.ValueOf(model)).FieldByName("UUID").Interface().(string)
+}
+
+func (odbi *ovndb) ormEqual(lhs, rhs Model, indexes ...string) (bool, error) {
+	api := odbi.client.ORM(odbi.db)
+	if lhs.Table() != rhs.Table() {
+		return false, nil
+	}
+	return api.Equal(lhs.Table(), lhs, rhs, indexes...)
+}
+
+// ormFindInCache looks for an item in the cache
+func (odbi *ovndb) ormFindInCache(table TableName, model Model, index ...string) (Model, error) {
+	odbi.cachemutex.RLock()
+	defer odbi.cachemutex.RUnlock()
+
+	tableCache, ok := odbi.ormCache[table]
+	if !ok {
+		return nil, ErrorNotFound
+	}
+
+	for _, elem := range tableCache {
+		eq, err := odbi.ormEqual(elem.(Model), model, index...)
+		if err != nil {
+			return nil, err
+		}
+		if eq {
+			return elem.(Model), nil
+		}
+	}
+	return nil, ErrorNotFound
 }
